787b15558600c0596e9ada82b8c94a0462519649
strider76 <manuel@betangible.com>
2016-09-28 15:50:57 +0200

    aadida nueva clase

---
+++ AnagramGame/src/com/toy/anagrams/lib/Clase.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.toy.anagrams.lib;
+
+/**
+ *
+ * @author strider
+ */
+public class Clase {
+    private int j;
+    
+    public Clase (int valor){
+        this.j=valor;
+    }
+    
+    public int getClase(){
+        return this.j;
+    }
+}
